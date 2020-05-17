d682d599abe42931d0dea6b54379bee54172a19d
WJJamshiD <wjjamshid@gmail.com>
2020-05-17 08:45:02 +0500

    Initial commit

---
+++ trynew/main/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify 
+import itertools
+from django.db.models.signals import pre_save
+# Create your models here.
+
+
+def upload_path(instan,file):
+	return '{0}/{1}'.format(instan.title,file)
+		
+
+class Post(models.Model):
+	title=models.CharField(max_length=50)
+	content=models.TextField()
+	timestamp=models.DateTimeField(auto_now=False,auto_now_add=True)
+	update=models.DateTimeField(auto_now_add=False,auto_now=True)
+	image=models.ImageField(upload_to=upload_path,height_field='height',width_field='width')
+	height=models.IntegerField(default=0)
+	width=models.IntegerField(default=0)
+	slug=models.SlugField(unique=True)
+
+
+
+	def __str__(self):
+		return self.title
+   
+	def get_absolute_url(self):
+		return reverse("post_detail", kwargs={"abc": self.id})
+	
+	#              creating unique slug (1)
+	# def generate_slug(self):
+	# 	max_length = self._meta.get_field('slug').max_length
+	# 	value = self.title
+	# 	slug_candidate = slug_original = slugify(value, allow_unicode=True)
+	# 	for i in itertools.count(1):
+	# 		if not Post.objects.filter(slug=slug_candidate).exists():
+	# 			break
+	# 		slug_candidate = '{}-{}'.format(slug_original, i)
+
+	# 	self.slug = slug_candidate
+
+	# def save(self, *args, **kwargs):
+	# 	if not self.pk:
+	# 		self.generate_slug()
+
+	# 	super().save(*args, **kwargs)
+
+
+
+	class Meta:
+		ordering=['-timestamp']
+
+
+
+
+#             creating unique slug (2)
+def create_myslug(instan,new_slug=None):
+	slug=slugify(instan.title)
+	if new_slug is not None:
+		slug=new_slug
+	qs=Post.objects.filter(slug=slug).order_by("-id")
+	if qs.exists():
+		new_slug='{}-{}'.format(slug,qs.first().id)
+		return create_myslug(instan,new_slug=new_slug)
+	return slug
+
+		
+def pre_save_post_receiver(sender,instance,*args,**kwargs):
+	if not instance.slug:
+		instance.slug=create_myslug(instance)
+
+pre_save.connect(pre_save_post_receiver,Post)
---
+++ trynew/main/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render,get_object_or_404,redirect
+from django.core.paginator import Paginator
+from .models import Post
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .froms import PostForm
+from django.contrib import messages 
+from django.db.models import Q
+# Create your views here.
+
+def post_list(request):
+    if request.method=='GET':
+        posts=Post.objects.all()
+        keyword=request.GET.get('qqq')
+        if keyword:
+            query=posts.filter(Q(title__icontains=keyword)|Q(content__icontains=keyword))
+            posts=query
+        paginator = Paginator(posts, 5)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        form=PostForm()
+        context={
+            'posts':posts,
+            'form':form,
+            'page_obj':page_obj,
+            'kw':keyword,
+            
+        }
+        return render(request,'index.html',context)
+    
+        
+
+def post_detail(request,abc=None):
+    if request.method=='GET':
+        #obj=get_object_or_404(Post,title=abc)
+        obj=Post.objects.get(id=abc)
+        context={
+            'post':obj,
+        }
+        return render(request,'detail.html',context)
+   
+
+def post_delete(request,abc=None):
+    if request.method=='GET':
+        if not request.user.is_staff and not request.user.is_superuser:
+            raise Http404
+        #obj=get_object_or_404(Post,title=abc)
+        obj=Post.objects.get(id=abc)
+        obj.delete()
+        messages.info(request,'Message deleted!')
+        return redirect('post_list')   
+
+def post_edit(request,abc=None):
+    post=Post.objects.get(id=abc)
+    form=PostForm(request.POST or None,request.FILES or None,instance=post)
+    if request.method=='GET':
+        context={
+            'form':form,
+        }
+        return render(request,'post_form.html',context)
+    if request.method=='POST':
+        
+        if form.is_valid():
+            data=form.save(commit=False)
+            data.save()
+            messages.success(request,'Succesfully edited!')
+            return HttpResponseRedirect(data.get_absolute_url())
+        else: 
+            messages.error(request,'Smth went wrong, try again!')
+            context={'form':form,
+                    }
+            return render(request, 'post_form.html',context)
+
+
+def post_create(request):
+    if request.method=='GET':
+        form=PostForm()
+        context={
+            'form':form,
+        }
+        return render(request,'post_form.html',context)
+    if request.method=='POST':
+        form=PostForm(request.POST or None,request.FILES or None)
+        if form.is_valid():
+            data=form.save(commit=False)
+            data.save()
+            messages.success(request,'Succesfully creted!')
+            return HttpResponseRedirect(data.get_absolute_url())
+        else: 
+            messages.error(request,'Smth went wrong, try again!')
+            context={'form':form,
+                    }
+            return render(request, 'post_form.html',context)
+
